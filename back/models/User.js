e9b54dcb07f67de41dca21319f56814fdd6b9ab4
kirnjiyun <kimjiyunee@naver.com>
2024-05-29 00:23:23 +0900

    로그인 성공 #1

--- back/models/User.js
+++ back/models/User.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const userSchema = Schema(
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+const userSchema = new Schema(
     {
         email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
@@ -9,14 +14,23 @@ const userSchema = Schema(
     },
     { timestamps: true }
 );
+
 userSchema.methods.toJSON = function () {
-    const obj = this._doc;
+    const obj = this.toObject();
     delete obj.password;
-    delete obj.__V;
-    delete obj.updateAt;
-    delete obj.createAt;
+    delete obj.__v;
+    delete obj.updatedAt;
+    delete obj.createdAt;
     return obj;
 };
 
+userSchema.methods.generateToken = async function () {
+    const token = await jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+        expiresIn: "1d",
+    });
+    return token;
+};
+
 const User = mongoose.model("User", userSchema);
+
 module.exports = User;
