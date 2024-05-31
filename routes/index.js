dae440c4cf9eefc7ddeb013e394337f1e70a43e6
kirnjiyun <kimjiyunee@naver.com>
2024-06-01 01:15:59 +0900

    heroku 포트 알아서 설정

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userApi = require("./user.api");
 const authApi = require("./auth.api");
+
 router.use("/user", userApi);
 router.use("/auth", authApi);
 
