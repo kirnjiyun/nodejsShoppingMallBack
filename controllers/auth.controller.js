cb2662f4c81a2e53bc6b3720be86ec9bbf55d9a2
kirnjiyun <kimjiyunee@naver.com>
2024-06-03 00:50:06 +0900

    로그아웃 관련 에러 고침

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,7 @@ const authController = {};
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 require("dotenv").config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
@@ -23,17 +24,16 @@ authController.loginWithEmail = async (req, res) => {
         res.status(400).json({ status: "fail", error: error.message });
     }
 };
+
 authController.authenticate = async (req, res, next) => {
     try {
         const tokenString = req.headers.authorization;
         if (!tokenString) throw new Error("Token not found");
 
         const token = tokenString.replace("Bearer ", "");
-        jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
-            if (error) throw new Error("Invalid token");
-            req.userId = payload._id;
-            next();
-        });
+        const payload = await promisify(jwt.verify)(token, JWT_SECRET_KEY);
+        req.userId = payload.id;
+        next();
     } catch (error) {
         res.status(400).json({ status: "fail", error: error.message });
     }
