3674e14235ad06063a91c57be41b75dad2c21e2f
kirnjiyun <kimjiyunee@naver.com>
2024-06-02 23:55:28 +0900

    로그인 토큰 #17

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,10 @@
 const authController = {};
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
 authController.loginWithEmail = async (req, res) => {
     try {
         const { email, password } = req.body;
@@ -8,7 +12,9 @@ authController.loginWithEmail = async (req, res) => {
         if (user) {
             const isMatch = await bcrypt.compare(password, user.password);
             if (isMatch) {
-                const token = await user.generateToken;
+                const token = jwt.sign({ id: user._id }, JWT_SECRET_KEY, {
+                    expiresIn: "1h",
+                });
                 return res.status(200).json({ status: "success", user, token });
             }
         }
@@ -17,4 +23,20 @@ authController.loginWithEmail = async (req, res) => {
         res.status(400).json({ status: "fail", error: error.message });
     }
 };
+authController.authenticate = async (req, res, next) => {
+    try {
+        const tokenString = req.headers.authorization;
+        if (!tokenString) throw new Error("Token not found");
+
+        const token = tokenString.replace("Bearer ", "");
+        jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+            if (error) throw new Error("Invalid token");
+            req.userId = payload._id;
+            next();
+        });
+    } catch (error) {
+        res.status(400).json({ status: "fail", error: error.message });
+    }
+};
+
 module.exports = authController;
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
-
+const authController = require("../controllers/auth.controller");
 // 회원가입
 router.post("/", userController.createUser);
-
+//토큰관련
+router.get(
+    "/me",
+    authController.authenticate, //validate한 토큰인지
+    userController.getUser //내 토큰 주세요
+);
 // GET 요청 처리 (예시)
 router.get("/", (req, res) => {
     res.send("User endpoint");
