e945f912f5b6bba3a0d957ff685e8ef3e9dbe870
kirnjiyun <kimjiyunee@naver.com>
2024-06-09 23:55:42 +0900

    상품 수정 삭제 #6

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -4,4 +4,9 @@ const authController = require("../controllers/auth.controller");
 const cartController = require("../controllers/cart.controller");
 router.post("/", authController.authenticate, cartController.addItemToCart);
 router.get("/", authController.authenticate, cartController.getCart);
+router.delete(
+    "/:id",
+    authController.authenticate,
+    cartController.deleteCartItem
+);
 module.exports = router;
