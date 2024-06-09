fecf43c7bdb6edeaa69e16b3411eec1189bad0f6
kirnjiyun <kimjiyunee@naver.com>
2024-06-10 00:05:30 +0900

    카트 #6

--- routes/cart.api.js
+++ routes/cart.api.js
@@ -9,4 +9,7 @@ router.delete(
     authController.authenticate,
     cartController.deleteCartItem
 );
+router.put("/:id", authController.authenticate, cartController.editCartItem);
+router.get("/qty", authController.authenticate, cartController.getCartQty);
+
 module.exports = router;
