18ba6789c6eca99480be4fcb9c89683b628f375a
kirnjiyun <kimjiyunee@naver.com>
2024-06-10 02:54:07 +0900

    카트 수정 #6

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -6,23 +6,16 @@ cartController.addItemToCart = async (req, res) => {
         const { userId } = req;
         const { productId, size, qty } = req.body;
 
-        // userId가 유효한지 확인
         if (!userId) {
             return res
                 .status(400)
                 .json({ status: "fail", error: "유효하지 않은 사용자입니다." });
         }
-
-        // 유저 가지고 카트 찾기
         let cart = await Cart.findOne({ userId: userId });
-
-        // 유저가 만든 카트가 없다면, 카트 만들어주기
         if (!cart) {
             cart = new Cart({ userId });
             await cart.save();
         }
-
-        // 이미 카트에 들어가 있는 아이템인지 확인
         const existItem = cart.items.find(
             (item) => item.productId.equals(productId) && item.size === size
         );
@@ -32,11 +25,8 @@ cartController.addItemToCart = async (req, res) => {
                 error: "아이템이 이미 카트에 존재합니다.",
             });
         }
-
-        // 카트에 아이템 추가
         cart.items.push({ productId, size, qty });
         await cart.save();
-
         res.status(200).json({
             status: "success",
             data: cart,
@@ -76,6 +66,7 @@ cartController.deleteCartItem = async (req, res) => {
         return res.status(400).json({ status: "fail", error: error.message });
     }
 };
+
 cartController.editCartItem = async (req, res) => {
     try {
         const { userId } = req;
@@ -99,6 +90,7 @@ cartController.editCartItem = async (req, res) => {
         return res.status(400).json({ status: "fail", error: error.message });
     }
 };
+
 cartController.getCartQty = async (req, res) => {
     try {
         const { userId } = req;
@@ -109,4 +101,5 @@ cartController.getCartQty = async (req, res) => {
         return res.status(400).json({ status: "fail", error: error.message });
     }
 };
+
 module.exports = cartController;
